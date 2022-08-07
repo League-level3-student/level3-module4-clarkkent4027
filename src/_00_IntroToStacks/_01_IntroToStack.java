838a8d1a0850e3c59a558e920540d4cc19073542
league <league@wts14-3.attlocal.net>
2022-08-07 11:26:23 -0700

    stack

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -6,30 +6,44 @@ import java.util.Stack;
 import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
-    public static void main(String[] args) {
-        // 1. Create a Stack of Doubles
-        //    Don't forget to import the Stack class
+	public static void main(String[] args) {
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
+		Stack<Double> Doub = new Stack<Double>();
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		Random r = new Random(100);
+		for (int i = 0; i < 100; i++) {
+			Doub.push(r.nextDouble() * 100);
+		}
 
-        // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		String num1 = JOptionPane.showInputDialog(null, "Please enter a number between 0 and 100");
+		String num2 = JOptionPane.showInputDialog(null, "Please enter another number between 0 and 100");
+		int Num1 = Integer.parseInt(num1);
+		int Num2 = Integer.parseInt(num2);
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
 
-        // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		for (int i = Doub.size(); i > 0; i--) {
+			Double d = Doub.pop();
+			if (d >= Num1 && d <= Num2) {
+				System.out.println(d);
+			}
 
-        // 4. Pop all the elements off of the Stack. Every time a double is popped that is
-        //    between the two numbers entered by the user, print it to the screen.
+		}
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
 
-
-        // EXAMPLE:
-        // NUM 1: 65
-        // NUM 2: 75
-
-        // Popping elements off stack...
-        // Elements between 65 and 75:
-        // 66.66876846
-        // 74.51651681
-        // 70.05110654
-        // 69.21350456
-        // 71.54506465
-        // 66.47984807
-        // 74.12121224
-    }
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
+	}
 }
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,5 +1,9 @@
 package _00_IntroToStacks;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class _02_TextUndoRedo {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
@@ -16,6 +20,16 @@ public class _02_TextUndoRedo {
      * pressed, the top Character is popped  off the Stack and added back to
      * the JLabel.
      */
+	public static void main(String[] args) {
+		
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel label = new JLabel();
+frame.add(panel);
+panel.add(label);
+frame.setVisible(true);
+frame.setSize(500, 500);
 
 
+	}
 }
