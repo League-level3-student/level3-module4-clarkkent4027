dcc593e560c030bb5486a1081ef090d062bf656e
league <league@imac-21.attlocal.net>
2022-08-21 11:29:03 -0700

    undoredo

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,14 +1,14 @@
 package _00_IntroToStacks;
 
-import java.awt.Component;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener {
 	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -23,25 +23,59 @@ public class _02_TextUndoRedo {
 	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
 	 * the top Character is popped off the Stack and added back to the JLabel.
 	 */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	String total = "";
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel();
-		
+		_02_TextUndoRedo a = new _02_TextUndoRedo();
+		a.setup();
+
+	}
+
+	void setup() {
 		frame.add(panel);
 		panel.add(label);
 		frame.setVisible(true);
 		frame.setSize(500, 500);
-		label.addKeyListener(null);
+		frame.addKeyListener(this);
 
 	}
 
+	@Override
 	public void keyPressed(KeyEvent e) {
+		Stack<String> text = new Stack<String>();
+		Stack<String> delete = new Stack<String>();
 
-		label.add
-		
+		String t = "" + e.getKeyChar();
+		total += t;
+		System.out.println(e.getKeyChar());
+		text.push(t);
+		label.setText(total);
+
+		if (e.getSource().equals(e.VK_BACK_SPACE)) {
+			total = total.substring(0, total.length() - 1);
+			System.out.println(total);
+			label.setText(total.substring(0, total.length() - 1));
+			delete.push(text.pop());
 
 		}
+		if (e.getSource().equals(e.VK_ENTER)) {
+			total += delete.pop();
+			label.setText(total);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
