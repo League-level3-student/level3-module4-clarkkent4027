308e485056a4d2d6938ec64d60180f7ceda1c5cf
league <league@imac-109.attlocal.net>
2022-09-04 11:25:51 -0700

    hang

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -2,6 +2,8 @@ package _03_Hangman;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -13,10 +15,16 @@ public class Hangman implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+
+	String werd = "";
+	int lives = 10;
+	String w = "";
+	String Live = "Lives: " + lives + " ";
 	Stack<String> words = new Stack<String>();
 
 	public static void main(String[] args) {
-
+		Hangman game = new Hangman();
+		game.game();
 	}
 
 	void setup() {
@@ -26,25 +34,57 @@ public class Hangman implements KeyListener {
 		frame.setSize(500, 500);
 		frame.addKeyListener(this);
 		String games = JOptionPane.showInputDialog("How many times would you like to play? (1-100)");
-		
+		for (int i = 0; i < games.length(); i++) {
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+			System.out.println(words.size());
+
+		}
+
+	}
+
+	void game() {
+		setup();
+		werd = words.pop();
+		for (int i = 0; i < werd.length(); i++) {
+			w += "_";
+		}
+		label.setText(Live + w);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		String s = "" + arg0.getKeyChar();
+		if (werd.contains(s)) {
+			w = w.substring(0, werd.indexOf(s)) + arg0.getKeyChar() + w.substring(werd.indexOf(s) + 1);
+			while(werd.indexOf(s)>-1) {
+				w = w.substring(0, werd.indexOf(s, werd.indexOf(s)+1)) + arg0.getKeyChar() + w.substring(werd.indexOf(s,werd.indexOf(s)) + 1);
+			}
+			System.out.println(w);
+			label.setText(Live + w);
+		} else {
+			lives = lives - 1;
+			Live = "Lives: " + lives + " ";
+			label.setText(Live + w);
+		}
+		if (lives == 0) {
+			JOptionPane.showMessageDialog(null, "Game Over! The word was " + werd + "!" );
+		}
+		if (w == werd) {
+			game();
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
